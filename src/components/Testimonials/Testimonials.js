2b13ec4c33e236fb40960920c856c222776d0712
Siphamandla <mr.smehlomakulu@gmail.com>
2020-03-31 09:24:01 +0200

    fixed the slide show, stopped using the state to manage slide and only used pure js to avoid having the page reload every time we change the slide

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -9,11 +9,6 @@ import portrait_two from '../../assets/images/portrait-two.jpg';
 import portrait_three from '../../assets/images/portrait-three.jpg';
 
 class Testimonials extends Component {
-    state = { 
-        currentSlide: 1,
-        actualCurrentSlide: 1,
-        numberOfSlides: 0,
-    }
 
     bootUpSlide = () => {
 
@@ -37,119 +32,135 @@ class Testimonials extends Component {
 
     componentDidMount() {
         this.bootUpSlide();
+        
+        let slideContainer = document.getElementById('carouselInner');
+        let slideStrip = slideContainer.lastChild;
+        let totalSlides = slideContainer.lastChild.children.length;
+        let nextButton = slideContainer.firstChild.firstChild;
+        let prevButton = slideContainer.firstChild.lastChild;
+        let portraitButtons = slideContainer.querySelectorAll('li');
+
+        nextButton.addEventListener('click', e => { this.nextSlide(e, totalSlides, portraitButtons) })
+        prevButton.addEventListener('click', e => { this.prevSlide(e, totalSlides, portraitButtons) })
+        
+        portraitButtons.forEach( button => button.addEventListener('click', e => {
+             this.slide(e, totalSlides, portraitButtons);
+        }))
+
+        //display first slide
+        slideStrip.style.transform = `translateX(-${ (1/totalSlides)*100 }%)`;
+
     }
 
 
-    slide = e => {
+    slide = ( e, totalSlides, portraitButtons) => {
         
         let slideStrip = e.target.offsetParent.offsetParent.children[2];
+        let currentSlide = parseInt( e.target.dataset.slideTo ) 
+
         slideStrip.style.transition = 'all ease-in-out 1000ms';
-        this.setState({ 
-            currentSlide: parseInt( e.target.dataset.slideTo ), 
-            actualCurrentSlide:  parseInt( e.target.dataset.slideTo ) 
-        })
+        slideStrip.style.transform = `translateX(-${ (currentSlide/totalSlides)*100 }%)`;
+
+        portraitButtons.forEach(button => button.classList.remove(classes.active))
+        e.target.classList.add(classes.active);
+
+        return currentSlide;
     }
 
-    nextSlide = (e) => {
+    nextSlide = (e, totalSlides, portraitButtons ) => {
+
         let slideStrip = e.currentTarget.offsetParent.lastChild;
-        console.log(slideStrip);
-        if( this.state.currentSlide > this.state.actualCurrentSlide ) {
 
+        let currentSlide;
+            portraitButtons.forEach( button => {
+                if(button.classList.contains(classes.active)){
+                    currentSlide = parseInt(button.dataset.slideTo);
+                }
+            } )
+
+        if( totalSlides - 2 === currentSlide ){
+            // console.log('We have reached the end, do something special or this shit will crush')
+            currentSlide++;
+            portraitButtons.forEach(button => button.classList.remove(classes.active));
             slideStrip.style.transition = 'all ease-in-out 1000ms';
-            this.setState({ currentSlide: 2, actualCurrentSlide: 2 });
-
-        } else if( this.state.currentSlide === (this.state.numberOfSlides - 2) ) {
-
-            this.setState( state => {
-                return { currentSlide: state.currentSlide++, actualCurrentSlide: 1 }
-            })
-
-            setTimeout(()=>{
-                slideStrip.style.transition = 'none';
-                slideStrip.style.transform = `translateX(-${( 1/this.state.numberOfSlides ) * 100 }%)`;
-            }, 1000)
-
-        } else if(this.state.currentSlide === 0 && this.state.actualCurrentSlide === (this.state.numberOfSlides-2)){
+            slideStrip.style.transform = `translateX(-${ (currentSlide/totalSlides)*100 }%)`;
+            portraitButtons[0].classList.add(classes.active);
             
-            slideStrip.style.transition = 'all ease-in-out 1000ms';
-            this.setState({ currentSlide: this.state.numberOfSlides-2 })
+            setTimeout( ()=>{
+                slideStrip.style.transition = 'none';
+                slideStrip.style.transform = `translateX(-${ (1/totalSlides)*100 }%)`;
+            } ,1000)
 
         } else {
-            this.setState( state => {
-                return { currentSlide: state.currentSlide++, actualCurrentSlide: state.actualCurrentSlide++ }
-            })
+            currentSlide++;
+            portraitButtons.forEach(button => button.classList.remove(classes.active))
+            slideStrip.style.transition = 'all ease-in-out 1000ms';
+            slideStrip.style.transform = `translateX(-${ (currentSlide/totalSlides)*100 }%)`;
+            portraitButtons[currentSlide-1].classList.add(classes.active);
         }
-        
     }
 
-    prevSlide = e => {
+    prevSlide = (e, totalSlides, portraitButtons) => {
+
         let slideStrip = e.currentTarget.offsetParent.lastChild;
-        console.log(slideStrip);
-        if( this.state.currentSlide === 1 && this.state.actualCurrentSlide === 1 ){
+        let currentSlide;
 
-            this.setState( state => {
-                return { currentSlide: state.currentSlide--, actualCurrentSlide: 3 };
-            })
+        portraitButtons.forEach( button => {
+            if(button.classList.contains(classes.active)){
+                currentSlide = parseInt(button.dataset.slideTo);
+            }
+        } )
 
-            setTimeout(()=>{
+        if( currentSlide === 1 ) {
+            currentSlide--;
+            portraitButtons.forEach(button => button.classList.remove(classes.active))
+            slideStrip.style.transition = 'all ease-in-out 1000ms';
+            slideStrip.style.transform = `translateX(-${ (currentSlide/totalSlides)*100 }%)`;
+            portraitButtons[totalSlides-3].classList.add(classes.active);
+
+            setTimeout( ()=>{
                 slideStrip.style.transition = 'none';
-                slideStrip.style.transform = `translateX(-${( 3/this.state.numberOfSlides ) * 100 }%)`;
-            }, 1000)
+                slideStrip.style.transform = `translateX(-${ ((totalSlides - 2 )/totalSlides)*100 }%)`;
+            } ,1000)
 
-        } else if ( this.state.currentSlide === 0 && this.state.actualCurrentSlide === 3 ) {
-
+        } else {
+            currentSlide--;
+            portraitButtons.forEach(button => button.classList.remove(classes.active))
             slideStrip.style.transition = 'all ease-in-out 1000ms';
-            this.setState({ currentSlide: 2, actualCurrentSlide: 2});
-        
-        } else if ( this.state.currentSlide === ( this.state.numberOfSlides - 1 )) {
-
-            slideStrip.style.transition = 'all ease-in-out 1000ms';
-            this.setState({ currentSlide: 1, actualCurrentSlide: 1 })
-
-        } else  {
-            this.setState( state => {
-                return { currentSlide: state.currentSlide--, actualCurrentSlide: state.actualCurrentSlide-- }
-            })
+            slideStrip.style.transform = `translateX(-${ (currentSlide/totalSlides)*100 }%)`;
+            portraitButtons[currentSlide-1].classList.add(classes.active);
         }
     }
 
     render() { 
-        const style = {
-            'transform': `translateX(-${ (this.state.currentSlide/this.state.numberOfSlides)*100 }%)`
-        }
 
         return ( 
             <Container id="testimonials">
                 <div className={classes.TestimonialsCarousel} id="carouselInner">
 
                     <div className={classes.slideButtons}>
-                        <div className={classes.next} onClickCapture={ this.nextSlide }>
+                        <div className={classes.next}>
                             <span><i className="fas fa-chevron-right"></i></span>
                         </div>
-                        <div className={classes.prev} onClick={ this.prevSlide }>
+                        <div className={classes.prev}>
                             <span><i className="fas fa-chevron-left"></i></span>
                         </div>
                     </div>
 
                     <ul className={classes.CarouselIndicators}>
                         <li 
-                            onClick={this.slide} 
                             data-slide-to={1} 
-                            className={ this.state.actualCurrentSlide === 1 ? classes.active : null }
+                            className={ classes.active }
                             style={{backgroundImage: `url(${portrait_one})`}}></li>
                         <li 
-                            onClick={this.slide} 
                             data-slide-to={2} 
-                            className={ this.state.actualCurrentSlide === 2 ? classes.active : null }
                             style={{backgroundImage: `url(${portrait_two})`}}></li>
                         <li 
-                            onClick={this.slide} 
                             data-slide-to={3} 
-                            className={ this.state.actualCurrentSlide === 3 ? classes.active : null }
                             style={{backgroundImage: `url(${portrait_three})`}}></li>
                     </ul>
 
-                    <div className={classes.CarouselInner} id="carouselStrip" style={style}>
+                    <div className={classes.CarouselInner} id="carouselStrip">
                         <Testimonial 
                             personName="3 Robert Nqwamla"
                             companyName="Finance Company"
