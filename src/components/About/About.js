9120773d079609f7ca825806208d77cf28035594
Siphamandla <mr.smehlomakulu@gmail.com>
2020-05-28 21:37:07 +0200

    Background-attachement set to initial on smaller devices

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,52 +4,72 @@ import scrollReveal from 'scrollreveal';
 
 import Container from '../UI/Container/Container';
 
-class About  extends Component {
-
+class About extends Component {
     state = {
-        containerWidth: 0 
-    }
+        containerWidth: 0,
+    };
 
     prepareComponent = () => {
         let container = document.querySelector('#aboutContainer');
         let backgroundImage = container.firstChild;
-        let containerWidth = container.offsetWidth; 
+        let containerWidth = container.offsetWidth;
 
         this.setState({ containerWidth: containerWidth });
 
         backgroundImage.style.width = containerWidth + 'px';
 
         //Make sure the size of the the background element is dynamically resizes to that of the container;
-        window.addEventListener( 'resize', () => {
+        window.addEventListener('resize', () => {
             this.setState({ containerWidth: container.offsetWidth });
-        })
-    }
+        });
+    };
 
     componentDidMount() {
-        this.prepareComponent(); 
+        this.prepareComponent();
     }
 
-    render(){
-        
-        // ScrollReveal animation 
-        const options = {distance: '100%',origin: 'left',delay: '500',duration: '1500'};
-        scrollReveal().reveal("#content", options);
+    render() {
+        // ScrollReveal animation
+        const options = {
+            distance: '100%',
+            origin: 'left',
+            delay: '500',
+            duration: '1500',
+        };
+        scrollReveal().reveal('#content', options);
         //-------------------------------
 
-        return ( 
-            <Container id="about">
-                <div className={classes.aboutUsContainer} id="aboutContainer">
-                    <div style={{ width: `${this.state.containerWidth}px` }} className={classes.backgroundImage}></div>
-                    <div className={classes.content} id="content">
+        return (
+            <Container id='about'>
+                <div className={classes.aboutUsContainer} id='aboutContainer'>
+                    <div
+                        style={{ width: `${this.state.containerWidth}px` }}
+                        className={classes.backgroundImage}
+                    ></div>
+                    <div className={classes.content} id='content'>
                         <h5>My story</h5>
                         <h1>I'm a freelancing Web designer and developer</h1>
-                        <p>When I started I never thought I would get hooked in this art, I was just trying to help a friend market her business. As a business student I knew one of the most effective ways was online marketing. We didn't have the money to pay for a website, so I did some research and came across a simple guideline on how to create a web page using HTML, next thing I was learning CSS. When I started using JavaScript I knew I found something interesting and I loved it. </p>
-                        <p>I created her website then moved on to create for other clients, from there I went on to learn other web design tools and technologies. </p>
+                        <p>
+                            When I started I never thought I would get hooked in
+                            this art, I was just trying to help a friend market
+                            her business. As a business student I knew one of
+                            the most effective ways was online marketing. We
+                            didn't have the money to pay for a website, so I did
+                            some research and came across a simple guideline on
+                            how to create a web page using HTML, next thing I
+                            was learning CSS. When I started using JavaScript I
+                            knew I found something interesting and I loved it.{' '}
+                        </p>
+                        <p>
+                            I created her website then moved on to create for
+                            other clients, from there I went on to learn other
+                            web design tools and technologies.{' '}
+                        </p>
                     </div>
                 </div>
             </Container>
         );
     }
 }
- 
+
 export default About;
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,24 +4,23 @@ import ContactForm from './ContactForm/ContactForm';
 import scrollReveal from 'scrollreveal';
 
 export const Contact = () => {
-
     const options = {
         distance: '150%',
         origin: 'left',
         delay: '500',
         duration: '1500',
-        easing: 'cubic-bezier(0.5, 0, 0, 1)'
+        easing: 'cubic-bezier(0.5, 0, 0, 1)',
     };
 
     scrollReveal().reveal('#contactForm', options);
-    
+
     return (
-        <div id="contact" className={classes.ContactContainer}>
-            <div id="contactForm" className={classes.ContainerInner}>
+        <div id='contact' className={classes.ContactContainer}>
+            <div id='contactForm' className={classes.ContainerInner}>
                 <ContactForm />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Contact;
