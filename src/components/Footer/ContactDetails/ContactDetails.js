34ca18e30ce318075aac5465bbcac8bd2425b4e2
Siphamandla <mr.smehlomakulu@gmail.com>
2020-07-02 16:24:01 +0200

    Correct email in the contact details

--- src/components/Footer/ContactDetails/ContactDetails.js
+++ src/components/Footer/ContactDetails/ContactDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './ContactDetails.module.css';
 
 const ContactDetails = props => {
-    return ( 
+    return (
         <div className={classes.ContactDetails}>
             <h2>Get in touch</h2>
             <ul>
@@ -10,10 +10,10 @@ const ContactDetails = props => {
                 <li><span className={classes.Bold}>P</span>: <a href="tel:+27781160376">+2778 116 0376</a></li>
                 <li><span className={classes.Bold}>P</span>: <a href="tel:+27783250866">+2778 325 0866</a></li>
                 <li><span className={classes.Bold}>E</span>: <a href="mailto:spha@netslate.co.za">spha@netslate.co.za</a></li>
-                <li><span className={classes.Bold}>E</span>: <a href="mailto:mr.smehlomakulu@gmail.com">mr.smehlomakulu@gamil.com</a></li>
+                <li><span className={classes.Bold}>E</span>: <a href="mailto:mr.smehlomakulu@gmail.com">mr.smehlomakulu@gmail.com</a></li>
             </ul>
         </div>
-     );
+    );
 }
- 
+
 export default ContactDetails;
