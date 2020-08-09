570ad7234877f870143febc0fb18d8a8b9a6db5a
Siphamandla <mr.smehlomakulu@gmail.com>
2020-08-09 08:13:58 +0200

    Add q-square project in gallery

--- src/components/Portfolio/Gallery/GalleryItemList.js
+++ src/components/Portfolio/Gallery/GalleryItemList.js
@@ -1,13 +1,15 @@
-import image1 from '../../../assets/images/gallery/gallery_1.jpg'
-import image2 from '../../../assets/images/gallery/gallery_2.jpg'
-import image3 from '../../../assets/images/gallery/gallery_3.jpg'
-import image4 from '../../../assets/images/gallery/gallery_4.jpg'
-import image5 from '../../../assets/images/gallery/gallery_5.jpg'
-import image6 from '../../../assets/images/gallery/gallery_6.jpg'
-import image7 from '../../../assets/images/gallery/gallery_7.jpg'
-import image8 from '../../../assets/images/gallery/gallery_8.jpg'
+import image0 from '../../../assets/images/gallery/gallery_0.png';
+import image1 from '../../../assets/images/gallery/gallery_1.jpg';
+import image2 from '../../../assets/images/gallery/gallery_2.jpg';
+import image3 from '../../../assets/images/gallery/gallery_3.jpg';
+import image4 from '../../../assets/images/gallery/gallery_4.jpg';
+import image5 from '../../../assets/images/gallery/gallery_5.jpg';
+import image6 from '../../../assets/images/gallery/gallery_6.jpg';
+import image7 from '../../../assets/images/gallery/gallery_7.jpg';
+import image8 from '../../../assets/images/gallery/gallery_8.jpg';
 
 export const images = [
+    { src: image0, caption: 'Q-Squared Concepts Website', url: 'http://www.q-squared.co.za/' },
     { src: image1, caption: 'Get Money Services billboard design' },
     { src: image2, caption: 'Get Money Services cup with company logo' },
     { src: image3, caption: 'Get Money Services logo mockup' },
