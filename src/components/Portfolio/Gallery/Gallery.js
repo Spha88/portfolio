f7f161ef365780f2e17343d3ace744625d4f38d6
Siphamandla <mr.smehlomakulu@gmail.com>
2020-08-09 07:48:08 +0200

    Add url to images in the gallery

--- src/components/Portfolio/Gallery/Gallery.js
+++ src/components/Portfolio/Gallery/Gallery.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { images } from './GalleryItemList';
 import Container from '../../UI/Container/Container';
 import './Gallery.module.css';
@@ -10,20 +10,24 @@ const Gallery = () => {
         const options = { distance: '100%', delay: '500', origin: 'bottom', duration: '1000', reset: false }
         scrollreveal().reveal('#item', options)
     })
-    
+
     return (
         <Container>
             <h2>Gallery</h2>
             <div className={" card-columns"} id="gallery"> {
-                images.map( (image, index) => {
+                images.map((image, index) => {
                     return (
                         <figure key={index} className="card" id="item">
-                            <img src={image.url} alt={`portfolio item ${index}`} className="card-img-top" />
-                            { image.caption ? (
+                            {image.url ?
+                                <a href={image.url}><img src={image.src} alt={`portfolio item ${index}`} className="card-img-top" /></a>
+                                : <img src={image.src} alt={`portfolio item ${index}`} className="card-img-top" />
+                            }
+
+                            {image.caption ? (
                                 <div className="card-body">
                                     <p className="card-text">{image.caption}</p>
                                 </div>
-                            ) : null }
+                            ) : null}
                         </figure>
                     );
                 })
--- src/components/Portfolio/Gallery/GalleryItemList.js
+++ src/components/Portfolio/Gallery/GalleryItemList.js
@@ -8,12 +8,12 @@ import image7 from '../../../assets/images/gallery/gallery_7.jpg'
 import image8 from '../../../assets/images/gallery/gallery_8.jpg'
 
 export const images = [
-    {url: image1, caption: 'Get Money Services billboard design'},
-    {url: image2, caption: 'Get Money Services cup with company logo'},
-    {url: image3, caption: 'Get Money Services logo mockup'},
-    {url: image4, caption: 'Saffron logo design presentation'},
-    {url: image5, caption: 'JAK Strydom WordPress Theme'},
-    {url: image6, caption: 'Family getaway pamphlet design'},
-    {url: image7, caption: 'ANOCS logo Design'},
-    {url: image8, caption: 'Covid-19 Statistics website'}
+    { src: image1, caption: 'Get Money Services billboard design' },
+    { src: image2, caption: 'Get Money Services cup with company logo' },
+    { src: image3, caption: 'Get Money Services logo mockup' },
+    { src: image4, caption: 'Saffron logo design presentation' },
+    { src: image5, caption: 'JAK Strydom WordPress Theme', url: 'http://jakstrydom.com/' },
+    { src: image6, caption: 'Family getaway pamphlet design' },
+    { src: image7, caption: 'ANOCS logo Design' },
+    { src: image8, caption: 'Covid-19 Statistics website' }
 ]
