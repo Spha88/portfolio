a95c0a6987d481395a547fc7c4102beebc87c916
Siphamandla <mr.smehlomakulu@gmail.com>
2020-08-09 07:34:03 +0200

    Change the a skill name from ECMAScript to JavaScript

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import classes from './Skills.module.css';
+import classes from './Skills.module.scss';
 
 import Container from '../UI/Container/Container';
 import Skill from './Skill/Skill';
 import skills from './skillsList';
 
 const Skills = props => {
-    return ( 
+    return (
         <Container id="skills">
             <h1 className={classes.SkillsHeading}>My Set of Skills</h1>
-            <div  className={classes.SkillsList}> {
+            <div className={classes.SkillsList}> {
                 skills.map((skill, index) => {
-                    return  <Skill 
-                                key={index}
-                                number={index} 
-                                skillHeading={skill.name} 
-                                experience={skill.experience} />
+                    return <Skill
+                        key={index}
+                        number={index}
+                        skillHeading={skill.name}
+                        experience={skill.experience} />
                 })
             } </div>
         </Container>
     );
 }
- 
+
 export default Skills;
