cb6cbe32cfbf2695e644be82cf96739d1bdb7e66
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-10-07 13:37:02 +0200

    Complete contact form

--- src/components/Contact/ContactForm/ContactForm.js
+++ src/components/Contact/ContactForm/ContactForm.js
@@ -1,100 +1,105 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import validator from 'validator';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import Fade from 'react-reveal/Fade';
+
+
 import classes from './ContactForm.module.scss';
-import TextInput from '../../UI/FormFields/TextInput/TextInput';
 import { Button50 } from '../../UI/Button50/Button50';
-import EmailInput from '../../UI/FormFields/EmailInput/EmailInput';
-import TextArea from '../../UI/FormFields/TextArea/TextArea';
 
-export default class ContactForm extends Component {
-    state = {
-        name: '',
-        email: '',
-        message: '',
-        errorMessage: '',
-        sent: false,
-    };
-    submitHandler = e => {
-        e.preventDefault();
-        let { name, email, message } = this.state;
-        if (
-            validator.isEmpty(name) ||
-            validator.isEmpty(email) ||
-            validator.isEmpty(message)
-        ) {
-            this.setState({ errorMessage: 'Fill in all the fields' });
-            return;
-        }
+const ContactForm = () => {
 
-        //Send email
-        let template_params = {
-            reply_to: email,
-            from_name: name,
-            to_name: 'Spha',
-            message_html: message,
-        };
+    const [sent, setSent] = useState(false);
+    const { register, handleSubmit, errors } = useForm();
 
-        let service_id = 'default_service';
-        let template_id = 'template_qgWb1eio';
-        window.emailjs.send(service_id, template_id, template_params);
-        //=============================================================
+    const onSubmit = data => {
+        axios({
+            method: 'post',
+            url: 'form_handler.php',
+            headers: { 'Content-type': 'application/json' },
+            data: {
+                name: data.name,
+                email: data.email,
+                subject: 'Email enquiry from the website',
+                message: data.message
+            }
+        })
+            .then(res => {
+                console.log('Message sent')
+                document.getElementById('contact-form').reset();
+                setSent(true);
+            })
+            .catch(error => {
+                console.log('Could not send message', error);
+            })
 
-        this.setState({
-            sent: true,
-            name: '',
-            email: '',
-            message: '',
-        });
-    };
-
-    updateContactFormDetails = (statePro, value) => {
-        let { name, email, message } = this.state;
-        this.setState({ [statePro]: value });
-        if (
-            !validator.isEmpty(name) ||
-            !validator.isEmpty(email) ||
-            !validator.isEmpty(message)
-        ) {
-            this.setState({ errorMessage: '' });
-        }
-    };
-
-    render() {
-        let { errorMessage, sent } = this.state;
-        return (
-            <div
-                className={`${classes.ContactFormContainer} ${
-                    sent && classes.sent
-                }`}
-            >
-                <h1>Get in touch</h1>
-                <div className={classes.ErrorMsgContainer}>
-                    {errorMessage && <small>{errorMessage}</small>}
-                </div>
-                <form onSubmit={this.submitHandler}>
-                    <TextInput
-                        sent={sent}
-                        label='Name'
-                        name='name'
-                        updateDetails={this.updateContactFormDetails}
-                    />
-                    <EmailInput
-                        sent={sent}
-                        label='Email'
-                        name='email'
-                        updateDetails={this.updateContactFormDetails}
-                    />
-                    <TextArea
-                        sent={sent}
-                        label='Message'
-                        updateDetails={this.updateContactFormDetails}
-                    />
-
-                    <div className={classes.SendBtnContainer}>
-                        <Button50 />
-                    </div>
-                </form>
-            </div>
-        );
     }
+
+    return (
+        <div className={`${classes.ContactFormContainer} ${sent && classes.sent}`}>
+
+            <h1>Get in touch</h1>
+
+            <form onSubmit={handleSubmit(onSubmit)} autoComplete="off" id="contact-form">
+
+                {/** Name */}
+                <div className={classes.InputFieldGroup}>
+                    <label htmlFor="name">Name</label>
+                    <input name="name" type="text"
+                        ref={register({
+                            required: { value: true, message: 'Name is required.' },
+                            minLength: { value: 2, message: 'Name too short.' },
+                            maxLength: { value: 60, message: 'Your name is too long.' },
+                            validate: value => validator.isAlpha(value.replace(/\s/g, '')) || 'Invalid characters, only alphabets allowed',
+                        })}
+                    />
+                    <div className={classes.ErrorMsg}>
+                        <Fade bottom when={!!(errors?.name?.message)}>
+                            <span>{errors?.name?.message}</span>
+                        </Fade>
+                    </div>
+                </div>
+
+                {/** Email */}
+                <div className={classes.InputFieldGroup}>
+                    <label htmlFor="email">Email</label>
+                    <input name="email" type="text"
+                        ref={register({
+                            required: { value: true, message: 'Email is required.' },
+                            validate: email => validator.isEmail(email) || 'Invalid E-mail'
+                        })}
+                    />
+                    <div className={classes.ErrorMsg}>
+                        <Fade bottom when={!!(errors?.email?.message)}>
+                            <span>{errors?.email?.message}</span>
+                        </Fade>
+                    </div>
+                </div>
+
+                {/** Message */}
+                <div className={classes.InputFieldGroup}>
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" cols="30" rows="6 "
+                        ref={register({
+                            required: { value: true, message: 'Message is required.' },
+                            minLength: { value: 2, message: 'Message too short.' },
+                            maxLength: { value: 1000, message: 'Your name is too long.' },
+                        })}
+                    ></textarea>
+                    <div className={classes.ErrorMsg}>
+                        <Fade bottom when={!!(errors?.message?.message)}>
+                            <span>{errors?.message?.message}</span>
+                        </Fade>
+                    </div>
+                </div>
+
+                <div className={classes.SendBtnContainer}>
+                    <Button50 />
+                </div>
+            </form>
+        </div>
+    );
 }
+
+export default ContactForm;
